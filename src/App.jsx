34e2ad33247d97a45e517aaa75fd7c534739f9b9
黃子耘 <81696168+DaDa-Patrick@users.noreply.github.com>
2025-12-05 19:37:32 +0800

    Switch to HashRouter for better GitHub Pages support

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
-// Placeholders for other pages
 import Preferences from './pages/Preferences';
 import Result from './pages/Result';
 import Favorites from './pages/Favorites';
@@ -31,17 +30,17 @@ function App() {
   }, []);
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="preferences" element={<Preferences />} />
-          <Route path="result" element={<Result />} />
-          <Route path="favorites" element={<Favorites />} />
-          <Route path="history" element={<History />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/preferences" element={<Preferences />} />
+          <Route path="/result" element={<Result />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
